bd364af34f4145a4dc020a978b6293a05afdb897
samjmarcena <89799529+samjmarcena@users.noreply.github.com>
2023-06-15 14:35:48 -0300

    Update LEDteste.sh

--- lab02/LEDteste.sh
+++ lab02/LEDteste.sh
@@ -3,7 +3,7 @@
 LED_GPIO=16  # Usar uma variavel facilita alteracoes futuras na porta usada
 
 echo $LED_GPIO >> "/sys/class/gpio/export"
-sleep .5
+sleep 1
 echo "out" >> "/sys/class/gpio/gpio$LED_GPIO/direction"
 echo $1 >> "/sys/class/gpio/gpio$LED_GPIO/value"
 sleep 3

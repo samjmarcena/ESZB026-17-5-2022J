bc77d8e7aee90fa6571d512a62c67d057b60fb4f
samjmarcena <89799529+samjmarcena@users.noreply.github.com>
2023-06-15 20:16:30 -0300

    Update sequencial.sh

--- lab02/sequencial.sh
+++ lab02/sequencial.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-LED_GPIO=16  # Usar uma variável facilita alterações futuras na porta usada
-VALUE=$1      # Argumento fornecido ao executar o script
+LED_AMARELO=16  # Usar uma variável facilita alterações futuras na porta usada
+LED_VERMELHO=20
+LED_VERDE=21
 
-echo $LED_GPIO > "/sys/class/gpio/export"
-sleep 1
-echo "out" > "/sys/class/gpio/gpio$LED_GPIO/direction"
-echo $VALUE > /sys/class/gpio/gpio$LED_GPIO/value
-sleep 3
-echo 0 > /sys/class/gpio/gpio$LED_GPIO/value
-echo $LED_GPIO > "/sys/class/gpio/unexport"
+VALUE=$1      # Argumento fornecido ao executar o script
+i=0
+
+echo $LED_AMARELO > "/sys/class/gpio/export"
+echo $LED_VERMELHO > "/sys/class/gpio/export"
+echo $LED_VERDE > "/sys/class/gpio/export"
+sleep .5
+
+while [i<5]
+  do
+    echo "out" > "/sys/class/gpio/gpio$LED_VERMELHO/direction"
+    echo $VALUE > "/sys/class/gpio/gpio$LED_VERMELHO/value"
+    sleep 2
+    echo 0 > "/sys/class/gpio/gpio$LED_VERMELHO/value"
+
+    echo "out" > "/sys/class/gpio/gpio$LED_VERDE/direction"
+    echo $VALUE > "/sys/class/gpio/gpio$LED_VERDE/value"
+    sleep 1
+    echo 0 > "/sys/class/gpio/gpio$LED_VERDE/value"
+
+    echo "out" > "/sys/class/gpio/gpio$LED_AMARELO/direction"
+    echo $VALUE > "/sys/class/gpio/gpio$LED_AMARELO/value"
+    sleep 1
+    echo 0 > "/sys/class/gpio/gpio$LED_AMARELO/value"
+
+    i=i+1
+  done
+  
+echo $LED_VERMELHO > "/sys/class/gpio/unexport"
+echo $LED_VERDE > "/sys/class/gpio/unexport"
+echo $LED_AMARELO > "/sys/class/gpio/unexport"

bf7ea1eb5daaac71e6e1112c43e70e0e2582490a
samjmarcena <89799529+samjmarcena@users.noreply.github.com>
2023-06-18 14:33:47 -0300

    Update ledteste.c

--- lab02/ledteste.c
+++ lab02/ledteste.c
@@ -1,16 +1,24 @@
+#include <stdio.h>
 #include <wiringPi.h>
 
-#define LED_AMARELO 16
+// LED_AMARELO Pin - wiringPi pin 27 is BCM_GPIO 16.
+// Check lib documentation to see the respectives Wiring pins to GPIO
 
-int main(void) {
-    //configuração inicial
-    wiringPiSetup();
-    pinMode(LED_AMARELO, OUTPUT);
+#define	LED_AMARELO	27
 
+int main (void)
+{
+  printf ("ta funcionando?\n") ;
 
-    digitalWrite(LED_AMARELO, HIGH);
-    delay(3000);
-    digitalWrite(LED_AMARELO, LOW);
-
-    return 0;
+  wiringPiSetup () ;
+  pinMode (LED_AMARELO, OUTPUT) ;
+  
+int i = 0;
+  while (i<5)
+  { 
+    digitalWrite (LED_AMARELO, HIGH) ;
+    delay (1000) ;
+    digitalWrite (LED_AMARELO, LOW) ;
+  }
+  return 0 ;
 }
